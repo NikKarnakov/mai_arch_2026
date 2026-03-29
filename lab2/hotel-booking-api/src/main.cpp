b893b4d350e8de80813fcd037c73c9f8ed650346
Nikita <115116690+NikKarnakov@users.noreply.github.com>
2026-03-30 00:05:47 +0300

    Create main.cpp

---
+++ lab2/hotel-booking-api/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "handlers.hpp"
+#include <userver/server/server.hpp>
+#include <userver/server/handlers/http_handler_base.hpp>
+
+int main() {
+    std::cout << "Стартуем... Если получится, конечно...\n";
+
+    userver::server::ServerConfig config;
+    config.port = 8080;
+    userver::server::Server server(config);
+
+    server.RegisterHandler("/auth/register", std::make_shared<AuthHandler>());
+    server.RegisterHandler("/auth/login", std::make_shared<LoginHandler>());
+    server.RegisterHandler("/hotels", std::make_shared<HotelHandler>());
+    server.RegisterHandler("/bookings", std::make_shared<BookingHandler>());
+
+    std::cout << "Запуск есть!\n";
+
+    server.Run();
+    return 0;
+}
