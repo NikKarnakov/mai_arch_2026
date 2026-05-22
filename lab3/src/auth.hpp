8d49fd7c1cc29047c3a7afe99b45e7e2c17b1de5
Nikita <115116690+NikKarnakov@users.noreply.github.com>
2026-05-22 22:58:22 +0300

    Create auth.hpp

---
+++ lab3/src/auth.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <optional>
+#include <string>
+
+namespace hotel::auth {
+
+std::string HashPassword(const std::string& password);
+bool CheckPassword(const std::string& password, const std::string& password_hash);
+
+std::string MakeToken(long user_id, const std::string& login);
+std::optional<long> ExtractUserId(const std::string& authorization_header);
+
+}
