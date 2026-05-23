b97cada0b1b0816c95f8278c0fd1c1059237190e
Nikita <115116690+NikKarnakov@users.noreply.github.com>
2026-05-23 12:50:35 +0300

    Update handlers.hpp

--- lab4/src/handlers.hpp
+++ lab4/src/handlers.hpp
@@ -1,28 +1,32 @@
 #pragma once
 
-#include "mongo_storage.hpp"
+#include "storage.hpp"
 
+#include <string_view>
+
+#include <userver/components/component_base.hpp>
+#include <userver/components/component_context.hpp>
 #include <userver/server/handlers/http_handler_json_base.hpp>
 
 namespace hotel {
 
-class BaseMongoHandler : public userver::server::handlers::HttpHandlerJsonBase {
+class BaseJsonHandler : public userver::server::handlers::HttpHandlerJsonBase {
 public:
-    BaseMongoHandler(const userver::components::ComponentConfig& config,
-                     const userver::components::ComponentContext& context);
+    BaseJsonHandler(const userver::components::ComponentConfig& config,
+                    const userver::components::ComponentContext& context);
 
 protected:
-    const MongoStorage& Storage() const;
+    const Storage& StorageRef() const;
 
 private:
-    const MongoStorage& storage_;
+    const Storage& storage_;
 };
 
-class MongoBookingsHandler final : public BaseMongoHandler {
+class RegisterHandler final : public BaseJsonHandler {
 public:
-    static constexpr std::string_view kName = "handler-mongo-bookings";
+    static constexpr std::string_view kName = "handler-register";
 
-    using BaseMongoHandler::BaseMongoHandler;
+    using BaseJsonHandler::BaseJsonHandler;
 
     userver::formats::json::Value HandleRequestJsonThrow(
         const userver::server::http::HttpRequest& request,
@@ -30,11 +34,72 @@ public:
         userver::server::request::RequestContext& context) const override;
 };
 
-class MongoReviewsHandler final : public BaseMongoHandler {
+class LoginHandler final : public BaseJsonHandler {
 public:
-    static constexpr std::string_view kName = "handler-mongo-reviews";
+    static constexpr std::string_view kName = "handler-login";
 
-    using BaseMongoHandler::BaseMongoHandler;
+    using BaseJsonHandler::BaseJsonHandler;
+
+    userver::formats::json::Value HandleRequestJsonThrow(
+        const userver::server::http::HttpRequest& request,
+        const userver::formats::json::Value& body,
+        userver::server::request::RequestContext& context) const override;
+};
+
+
+class UsersHandler final : public BaseJsonHandler {
+public:
+    static constexpr std::string_view kName = "handler-users";
+
+    using BaseJsonHandler::BaseJsonHandler;
+
+    userver::formats::json::Value HandleRequestJsonThrow(
+        const userver::server::http::HttpRequest& request,
+        const userver::formats::json::Value& body,
+        userver::server::request::RequestContext& context) const override;
+};
+
+class HotelsHandler final : public BaseJsonHandler {
+public:
+    static constexpr std::string_view kName = "handler-hotels";
+
+    using BaseJsonHandler::BaseJsonHandler;
+
+    userver::formats::json::Value HandleRequestJsonThrow(
+        const userver::server::http::HttpRequest& request,
+        const userver::formats::json::Value& body,
+        userver::server::request::RequestContext& context) const override;
+};
+
+class RoomsHandler final : public BaseJsonHandler {
+public:
+    static constexpr std::string_view kName = "handler-rooms";
+
+    using BaseJsonHandler::BaseJsonHandler;
+
+    userver::formats::json::Value HandleRequestJsonThrow(
+        const userver::server::http::HttpRequest& request,
+        const userver::formats::json::Value& body,
+        userver::server::request::RequestContext& context) const override;
+};
+
+class BookingsHandler final : public BaseJsonHandler {
+public:
+    static constexpr std::string_view kName = "handler-bookings";
+
+    using BaseJsonHandler::BaseJsonHandler;
+
+    userver::formats::json::Value HandleRequestJsonThrow(
+        const userver::server::http::HttpRequest& request,
+        const userver::formats::json::Value& body,
+        userver::server::request::RequestContext& context) const override;
+};
+
+class ReviewsHandler final : public BaseJsonHandler {
+public:
+    static constexpr std::string_view kName = "handler-reviews";
+
+    using BaseJsonHandler::BaseJsonHandler;
 
     userver::formats::json::Value HandleRequestJsonThrow(
         const userver::server::http::HttpRequest& request,
